18c4797a02405979f070ab0a6a0b3c536f33ace5
manojm01 <69072494+manojm01@users.noreply.github.com>
2022-01-23 10:30:17 +0530

    Time: 1 ms (50.00%), Space: 39.5 MB (12.50%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -1,33 +1,37 @@
 class Solution {
     public int countElements(int[] a) {
-        int n = a.length;
-        Arrays.sort(a);
-        int ans=0;
-        int i=1;
-        int j=n-1;
-         if(n<=2)return 0;
-        while(i<n && a[i]==a[i-1])i++;
-        while(j>0 && a[j]==a[j-1])j--;
-        if(i==n || j==-1)return 0;
-        System.out.println(a[i]);
-        System.out.println(a[j]);
-        
-        for(int k=i;k<j;k++){
-            if(a[k]>a[k-1]){
-                while(k<n && a[k]==a[k+1]){
-                    k++;
-                    
-                    ans++; 
-                    // if(j==n-1)return 0;
-                }
-                // if(j==n-1)return 0;
-                if(a[k]<a[k+1]){
-                   ans++; 
-                }
-                
-            }
+        int ans = 0;
+        int max = a[0];
+        int min = a[0];
+        for(int i:a){
+            max = Math.max(i,max);
+            min = Math.min(i,min);
         }
-        if(n<=2)return 0;
+        for(int i:a){
+            if(i>min && i<max)ans++;
+        }
         return ans;
     }
 }
+
+// Arrays.sort(a);
+//         int ans=0;
+//         int i=1;
+//         int j=n-1;
+//          if(n<=2)return 0;
+//         while(i<n && a[i]==a[i-1])i++;
+//         while(j>0 && a[j]==a[j-1])j--;
+//         if(i==n || j==-1)return 0;
+
+//         for(int k=i;k<j;k++){
+//             if(a[k]>a[k-1]){
+//                 while(k<n && a[k]==a[k+1]){
+//                     k++;
+//                     ans++; 
+//                 }
+//                 if(a[k]<a[k+1]){
+//                    ans++; 
+//                 }
+//             }
+//         }
+//         if(n<=2)return 0;
