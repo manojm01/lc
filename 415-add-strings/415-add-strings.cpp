80ea0290cad183563b50ae58bece15420fabf341
manojm01 <69072494+manojm01@users.noreply.github.com>
2022-01-28 11:49:26 +0530

    Time: 48 ms (7.37%), Space: 55.7 MB (20.23%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -3,12 +3,10 @@ public:
     string addStrings(string s1, string s2) {
         int n = s1.length();
         int m = s2.length();
-        
-        vector<char>v;
         int carry=0;
-        
         int i=n-1,j=m-1,in=0;
         string ans = "";
+        
         while(i>=0 || j>=0 || carry){
             int sum=0;
             if(i>=0) sum += (s1[i--]-'0');
