f8481eef0c0147873a4addc88564606ba4945c8b
manojm01 <69072494+manojm01@users.noreply.github.com>
2022-01-31 13:19:25 +0530

    Added solution - LeetHub

---
+++ Cousins of a given node - GFG/cousins-of-a-given-node.java
@@ -0,0 +1,171 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+} class GfG {
+    public static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+    
+    public static Node point(Node root, int n)
+    {
+        if(root == null)
+            return null;
+            
+        if(root.data == n)
+            return root;
+            
+        Node l = point(root.left, n);
+        if(l!=null && l.data==n)
+            return l;
+            
+        Node r= point(root.right, n);
+        return r;
+        
+        
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            Node p = point(root,n);
+            
+            Solution ob=new Solution();
+            ArrayList<Integer> ans= ob.printCousins(root, p);
+            
+            for(int i=0;i<ans.size();i++)
+            {
+                    System.out.print(ans.get(i)+" ");
+    
+            }
+            
+            System.out.println();
+            
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+} */
+
+class Solution
+{
+    public static ArrayList<Integer> printCousins(Node root, Node t)
+    {
+        //code here
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        ans.add(-1);
+        if(root==null || root==t)return ans;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        boolean f = false;
+        while(q.size()!=0){
+            int s = q.size();
+            
+            for(int i=1;i<=s;i++){
+                Node temp = q.poll();
+                if(temp.left==t || temp.right==t){
+                    f=true;
+                }
+                else{
+                    if(temp.left!=null)q.add(temp.left);
+                    if(temp.right!=null)q.add(temp.right);
+                }
+            }
+            if(f){
+                ArrayList<Integer> l = new ArrayList<Integer>();
+                while(!q.isEmpty()) {
+                    l.add(q.poll().data);
+                }  
+                if(l.size()==0)l.add(-1);
+                return l;
+            }
+        }
+        return ans;
+    }
+}
