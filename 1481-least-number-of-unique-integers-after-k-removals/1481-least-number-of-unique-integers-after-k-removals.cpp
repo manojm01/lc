f33265e2f0101938e8be128e604e2d2a011df72b
manojm01 <69072494+manojm01@users.noreply.github.com>
2022-01-31 15:39:54 +0530

    Time: 345 ms (23.67%), Space: 62.8 MB (48.17%) - LeetHub

---
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& a, int k) {
+        map<int,int>m;
+        for(int i:a){
+            m[i]++;
+        }
+        int ms = m.size();
+        int j=0;
+        vector<int>f(ms);
+        for(auto it:m){
+            f[j++]=it.second;
+        }
+        sort(f.begin(),f.end());
+        int c=0;
+        
+        do{
+         k = k-f[c];
+            if(k>=0){
+                c++;
+            }
+            else{
+                break;
+            }
+        }while(k>0);
+        return ms-c;
+    }
+};
