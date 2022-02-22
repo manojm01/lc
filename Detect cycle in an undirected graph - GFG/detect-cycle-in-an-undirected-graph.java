e11b93891ce4574a58ab9782e2a159b4c740c348
manojm01 <69072494+manojm01@users.noreply.github.com>
2022-02-22 17:51:25 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,84 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+class Node{
+    int first;
+    int second;
+    public Node(int first,int second){
+        this.first = first;
+        this.second = second;
+        
+    }
+}
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int v, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+         boolean vis[] = new boolean[v];
+         Arrays.fill(vis,false);
+         
+         for(int i=0;i<v;i++){
+            if(vis[i]==false){ 
+                vis[i]=true;
+               if(solve(i,vis,adj)){
+                   return true;
+               }
+            } 
+         }
+         
+        return false;
+    }
+    public boolean solve(int s,boolean vis[],ArrayList<ArrayList<Integer>> adj){
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(s,-1));
+        vis[s] = true;
+        
+        while(!q.isEmpty()){
+            int node = q.peek().first;
+            int prev = q.peek().second;
+            q.remove();
+            for(int i:adj.get(node)){
+                if(vis[i]==false){
+                    q.add(new Node(i,node));
+                    vis[i] = true;
+                }
+                else if(prev != i){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+}
