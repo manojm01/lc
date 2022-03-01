d17e6657aaa2f3641e6d58af78cc02917d54eea1
manojm01 <69072494+manojm01@users.noreply.github.com>
2022-03-01 12:42:47 +0530

    Time: 115 ms (72.71%), Space: 207.8 MB (32.55%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int s, int d) {
+        
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int x = edges.length;
+        
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<x;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        int vis[] = new int[n];
+        for(int i=0;i<n;i++){
+             if(vis[i]==0)
+                if(check(i,adj,s,d,vis)){
+                    return true;
+                }
+            if(vis[s]==1&&vis[d]==1)return true;
+            vis[s]=0;vis[d]=0;
+        }
+        return false;
+    }
+     public boolean check(int node, ArrayList<ArrayList<Integer>> adj, int s, int d, int[] vis){
+         boolean ss = false;
+         vis[node]=1;
+         
+         if(vis[s]==1&&vis[d]==1)return true;
+             
+         for(int i:adj.get(node)){
+             if(vis[i]==0)
+              if(check(i,adj,s,d,vis))return true;
+         }
+         
+         return false;
+     }
+        
+}
