c8eee5ea4c9965459be593ba5e6b1610a3de5cfb
manojm01 <69072494+manojm01@users.noreply.github.com>
2022-03-03 23:23:10 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (37.06%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -25,3 +25,18 @@ class Solution {
         return ans;
     }
 }
+        // int n = nums.length;
+        // if (n<3){
+        //     return 0;
+        // }
+        // int ans=0;
+        // int[] dp = new int[n];
+        // for(int i=2;i<n;i++){
+        //     if ((nums[i]-nums[i-1])==(nums[i-1]-nums[i-2])){
+        //         dp[i] = dp[i-1]+1;
+        //         ans+=dp[i];
+        //     }
+        // }
+        // return ans;
+
+
