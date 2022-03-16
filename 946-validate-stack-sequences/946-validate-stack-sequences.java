667e89711492eba2ce3420fada03e220dd176e92
manojm01 <69072494+manojm01@users.noreply.github.com>
2022-03-16 16:05:47 +0530

    Time: 5 ms (43.81%), Space: 45.2 MB (43.80%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -3,16 +3,11 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         int in=0;
         for(int i:p1){
-           
-            if(st.size()!=0&&st.peek()==p2[in]){
-                
-                while(st.size()!=0&&st.peek()==p2[in]){
+           while(st.size()!=0&&st.peek()==p2[in]){
                      st.pop();
                     in++;
-                }
-               
-            }
-             st.push(i);
+           }
+           st.push(i);
         }
         while(st.size()!=0){
             if(st.peek()!=p2[in]){
