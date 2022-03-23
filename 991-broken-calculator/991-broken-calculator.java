e89c2a06288f56ffe57dce5f466b7141042eb3bd
manojm01 <69072494+manojm01@users.noreply.github.com>
2022-03-23 23:40:10 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (80.88%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int brokenCalc(int s, int t) {
+        int ans = 0;
+        
+        while(t>s){
+            if(t%2==0){
+               t/=2;
+            }
+            else{
+                t++;
+            }
+            ans++;
+        }
+        return ans+s-t;
+    }
+}
