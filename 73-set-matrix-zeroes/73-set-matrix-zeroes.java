7f334f49142fb569aacd5c6905dc2ea13d8a5080
Manoj Metgud <69072494+manojm01@users.noreply.github.com>
2022-06-14 12:38:25 +0530

    Time: 1 ms (94.85%), Space: 43.5 MB (97.64%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,32 +1,84 @@
 class Solution {
     public void setZeroes(int[][] a) {
-        ArrayList<Integer> r = new ArrayList<>();
-        ArrayList<Integer> c = new ArrayList<>();
+        int n = a.length;
+        int m = a[0].length;
+        //   for (int i = 0; i < a.length; i++) {
+        //     for (int j = 0; j < a[0].length; j++) {
+        //         System.out.print(a[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        boolean checkR=false;
+        boolean checkC=false;
         
-        for(int i=0;i<a.length;i++){
-            for(int j=0;j<a[0].length;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(a[i][j]==0){
-                    if(!r.contains(i))r.add(i);
-                    if(!c.contains(j))c.add(j);
+                    if(i==0)checkR=true;
+                    if(j==0)checkC=true;
+                    a[i][0]=0;
+                    a[0][j]=0;
                 }
             }
         }
-
-        for(int i:r){
-            solve_r(a,i);
+         for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(a[i][0]==0 || a[0][j]==0){
+                   a[i][j]=0;
+                }
+            }
         }
-        for(int i:c){
-            solve_c(a,i);
+        if(checkR){
+            for(int i=0;i<m;i++){
+                a[0][i]=0;
+            }
         }
-    }
-    public void solve_r(int[][] a,int r){
-        for(int j=0;j<a[0].length;j++){
-            a[r][j]=0;
+        if(checkC){
+            for(int i=0;i<n;i++){
+                a[i][0]=0;
+            }
         }
-    }
-    public void solve_c(int[][] a,int c){
-        for(int j=0;j<a.length;j++){
-            a[j][c]=0;
-        }
+        
+        
+        // System.out.println("-------");
+        //  for (int i = 0; i < a.length; i++) {
+        //     for (int j = 0; j < a[0].length; j++) {
+        //         System.out.print(a[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
+
+// class Solution {
+//     public void setZeroes(int[][] a) {
+//         ArrayList<Integer> r = new ArrayList<>();
+//         ArrayList<Integer> c = new ArrayList<>();
+        
+//         for(int i=0;i<a.length;i++){
+//             for(int j=0;j<a[0].length;j++){
+//                 if(a[i][j]==0){
+//                     if(!r.contains(i))r.add(i);
+//                     if(!c.contains(j))c.add(j);
+//                 }
+//             }
+//         }
+
+//         for(int i:r){
+//             solve_r(a,i);
+//         }
+//         for(int i:c){
+//             solve_c(a,i);
+//         }
+//     }
+//     public void solve_r(int[][] a,int r){
+//         for(int j=0;j<a[0].length;j++){
+//             a[r][j]=0;
+//         }
+//     }
+//     public void solve_c(int[][] a,int c){
+//         for(int j=0;j<a.length;j++){
+//             a[j][c]=0;
+//         }
+//     }
+// }
