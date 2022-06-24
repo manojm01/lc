dfc176bb90187b66f589748a78f82f7b45b15ee8
Manoj Metgud <69072494+manojm01@users.noreply.github.com>
2022-06-24 15:30:07 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (41.83%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -1,32 +1,34 @@
 class Solution {
     public int[][] generateMatrix(int n) {
         int ans[][] = new int[n][n];
+           
         int rb=0,re=n-1,cb=0,ce=n-1;
         int c=1;
         while(rb<=re && cb<=ce){
-            
-            for(int i=cb;i<=ce;i++){   //right
-                ans[rb][i] = c++;
+            for(int i=cb;i<=ce;i++){
+                ans[rb][i] = c;
+                c++;
             }
             rb++;
-            
-            for(int i=rb;i<=re;i++){  // down
-                ans[i][ce] = c++;
+            for(int i=rb;i<=re;i++){
+                ans[i][ce] = c;
+                c++;
             }
             ce--;
             
-            if(rb<=re){
-                for(int i=ce;i>=cb;i--){   //left
-                    ans[re][i] = c++;
-                }
-                re--;
+            if(rb<=re)
+            for(int i=ce;i>=cb;i--){
+                ans[re][i] = c;
+                c++;
             }
-            if(cb<=ce){
-                for(int i=re;i>=rb;i--){  //top
-                    ans[i][cb] = c++;
-                }
-                cb++;
+            re--;
+            
+            if(cb<=ce)
+            for(int i=re;i>=rb;i--){
+                ans[i][cb] = c;
+                c++;
             }
+            cb++;
         }
         
         return ans;
