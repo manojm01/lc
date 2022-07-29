791f7c11838bfbf11c204d35e75efc84a3b032c1
Manoj Metgud <69072494+manojm01@users.noreply.github.com>
2022-07-29 16:22:13 +0530

    Time: 12 ms (18.71%), Space: 8.5 MB (53.91%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -7,12 +7,12 @@ public:
         vector<string> ans;
         for(int i=0;i<n;i++){
             unordered_map<char,char>m;
-             unordered_map<char,int>mr;
+            unordered_map<char,int>mr;
             int x = w[i].size();
             if(ps!=x)continue;
-             bool f = false;
+            bool f = false;
+            
             for(int j=0;j<x;j++){
-                
                 if(m.find(p[j])==m.end()){
                     if(mr[w[i][j]]==1){
                        f = true;
