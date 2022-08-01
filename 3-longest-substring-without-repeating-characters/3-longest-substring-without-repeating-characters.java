36035e9cf5b0beff2680948c783aaa345b7321c3
Manoj Metgud <69072494+manojm01@users.noreply.github.com>
2022-08-01 12:27:10 +0530

    Time: 13 ms (51.84%), Space: 45.2 MB (44.85%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -2,18 +2,32 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
         if(n==0)return 0;
-        int ans = 1;
-        for(int i=0;i<n;i++){
-            HashSet<Character> set = new HashSet<>();
-            set.add(s.charAt(i));
-            int cnt=1;
-            for(int j=i+1;j<n;j++){
-                if(set.contains(s.charAt(j)))break;
-                set.add(s.charAt(j));
-                cnt++;
-                ans = Math.max(ans,cnt);
+        HashMap<Character,Integer> map = new HashMap<>();
+        int i=0,j=0;
+        int ans = 0;
+        char c[] = s.toCharArray();
+        
+        for(j=0;j<n;j++){
+            if(map.containsKey(c[j])){
+                i = Math.max(i,map.get(c[j])+1);
             }
+            ans = Math.max(ans,j-i+1);
+            map.put(c[j],j);
         }
         return ans;
     }
 }
+
+        //int ans = 1;
+        // for(int i=0;i<n;i++){
+        //     HashSet<Character> set = new HashSet<>();
+        //     set.add(s.charAt(i));
+        //     int cnt=1;
+        //     for(int j=i+1;j<n;j++){
+        //         if(set.contains(s.charAt(j)))break;
+        //         set.add(s.charAt(j));
+        //         cnt++;
+        //         ans = Math.max(ans,cnt);
+        //     }
+        // }
+        // return ans;
