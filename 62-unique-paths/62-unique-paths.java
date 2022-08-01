cdac08ea19ed7784e1074cd6b6d3fe4c615ce64a
Manoj Metgud <69072494+manojm01@users.noreply.github.com>
2022-08-01 23:42:33 +0530

    Time: 1 ms (55.39%), Space: 41.6 MB (10.24%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,16 +1,17 @@
 class Solution {
-    int mod = (int)2*(int)(1e9);
+   
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
+        int dp[][] = new int[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++)
                 dp[i][j]=-1;
         }
-        return solve(m-1,n-1,dp);
+        
+        return solve(m,n,dp);
     }
     public int solve(int m,int n,int dp[][]){
-        if(n==0 && m==0)return 1;
-        if(m<0 || n<0)return 0;
+        if(n==1 && m==1)return 1;
+        if(m<1 || n<1)return 0;
         if(dp[m][n]!=-1)return dp[m][n];
         
         return dp[m][n]=solve(m-1,n,dp)+solve(m,n-1,dp);
